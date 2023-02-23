e9399221c0c3f0d004374fcf4aab9d20fa8fc79b
Zelong Wang <zelong1222@outlook.com>
2023-02-23 02:48:19 -0600

    Add logo

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 const introText = document.querySelector('.introtext');
-const introSubText = document.querySelector('.introsubtext');
-const arrows = document.querySelector('.arrow-container');
 
 window.onload = () => {
     introText.style.opacity = 1;
-    setTimeout(() => {
-        introSubText.style.opacity = 1;
-        arrows.style.opacity = 1;
-    }, 1500);
 };
