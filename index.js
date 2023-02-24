9c85f7bc4f593d4854bb17d37bc472daa06f18a1
Zelong Wang <zelong1222@outlook.com>
2023-02-24 11:24:40 -0600

    update video logic

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const introText = document.querySelector('.introtext');
-let firstTimePlay = false;
-window.onload = () => {
-    introText.style.opacity = 1;
-};
-
 var video = document.getElementById("background-video");
-var image = document.getElementById("background-image");
+let firstTimePlay = true;
+if (firstTimePlay) {
+    introText.style.opacity = 1;
+    video.currentTime = 0.5;
+}
 
 video.addEventListener("canplaythrough", function() {
-    if (!firstTimePlay) {
-        video.currentTime = 0.5;
-        video.play();
-        setTimeout(function(){image.style.display = "none";}, 100);
-        firstTimePlay = true;
-    }
+    video.play();
+    firstTimePlay = false;
 });
