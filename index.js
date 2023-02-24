3fc4dbb610f6165972cce91416b92aaa2c8b2cca
Zelong Wang <zelong1222@outlook.com>
2023-02-24 00:08:24 -0600

    update new home page video

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const introText = document.querySelector('.introtext');
-
+let firstTimePlay = false;
 window.onload = () => {
     introText.style.opacity = 1;
 };
+
+var video = document.getElementById("background-video");
+var image = document.getElementById("background-image");
+
+video.addEventListener("canplaythrough", function() {
+    image.style.display = "none";
+    if (!firstTimePlay) {
+        video.currentTime = 1.25;
+        firstTimePlay = true;
+    }
+    video.play();
+});
