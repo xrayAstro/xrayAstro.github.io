c5b720e0a9d29dd451f67b7b1ff46298b567a501
Zelong Wang <zelong1222@outlook.com>
2023-03-02 21:47:44 -0600

    Update people.js

--- people.js
+++ people.js
@@ -1,20 +1,30 @@
 const people = [
-  { name: "Dan McCammon", email: "mccammon@physics.wisc.edu", elementId: "dan" },
-  { name: "Felix Jaeckel", email: "jaeckel2@wisc.edu", elementId: "felix" },
-  { name: "Avirup Roy", email: "aroy22@wisc.edu", elementId: "avirup" },
-  { name: "C. V. Ambarish", email: "ambarish@wisc.edu", elementId: "ambarish" },
-  { name: "Miriam Marino", email: "marino6@wisc.edu", elementId: "miriam" },
-  { name: "Sophia", email: "srnowak2@wisc.edu", elementId: "sophia" },
-  { name: "Wei Liu", email: "wliu345@wisc.edu", elementId: "wei" },
-  { name: "Shaula Yan", email: "yan99@wisc.edu", elementId: "shuala" },
-  { name: "Elisa John", email: "ecjohn@wisc.edu", elementId: "elisa" },
-  { name: "Zelong Wang", email: "wang2447@wisc.edu", elementId: "zelong" },
+  { email: "109-99-99-97-109-109-111-110-64-112-104-121-115-105-99-115-46-119-105-115-99-46-101-100-117", elementId: "dan" },
+  { email: "106-97-101-99-107-101-108-50-64-119-105-115-99-46-101-100-117", elementId: "felix" },
+  { email: "97-114-111-121-50-50-64-119-105-115-99-46-101-100-117", elementId: "avirup" },
+  { email: "97-109-98-97-114-105-115-104-64-119-105-115-99-46-101-100-117", elementId: "ambarish" },
+  { email: "109-97-114-105-110-111-54-64-119-105-115-99-46-101-100-117", elementId: "miriam" },
+  { email: "115-114-110-111-119-97-107-50-64-119-105-115-99-46-101-100-117", elementId: "sophia" },
+  { email: "119-108-105-117-51-52-53-64-119-105-115-99-46-101-100-117", elementId: "wei" },
+  { email: "121-97-110-57-57-64-119-105-115-99-46-101-100-117", elementId: "shuala" },
+  { email: "101-99-106-111-104-110-64-119-105-115-99-46-101-100-117", elementId: "elisa" },
+  { email: "119-97-110-103-50-52-52-55-64-119-105-115-99-46-101-100-117", elementId: "zelong" },
 ];
+
+// Decryption function
+function decryptEmail(encryptedEmail) {
+  let decrypted = '';
+  const emailChars = encryptedEmail.split('-');
+  for (let i = 0; i < emailChars.length; i++) {
+    decrypted += String.fromCharCode(parseInt(emailChars[i]));
+  }
+  return decrypted;
+}
 
 people.forEach(function(person) {
   const emailElement = document.getElementById(person.elementId);
   emailElement.onclick = function() {
-    window.location.href = "mailto:" + person.email;
+    window.location.href = "mailto:" + decryptEmail(person.email);
   };
 });
 
